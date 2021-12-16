dd2fa074dcb76685a529c7612b786a7ce7d2e1ce
NagibatoSs <zhuravleva_02@mail.ru>
2021-12-16 12:56:26 +0400

    test2

---
+++ REALtor/REALtor/Data/Interfaces/IAllHouses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using REALtor.Data.Models;
+
+namespace REALtor.Data.Interfaces
+{
+    public interface IAllHouses
+    {
+        IEnumerable<House> Houses { get; }
+        IEnumerable<House> GetAvailableHouses { get; }
+        House getObjectHouse(int houseId);
+    }
+}
